2fa3eb0132106933f0278b91774da0264c99b4e7
syam <84723010+syamsuri09@users.noreply.github.com>
2023-01-28 22:16:19 +0530

    import java.util.Scanner;
    class hello  {
        public static void main(String[] args){
            
            Scanner input = new Scanner(System.in);
            String line = input.nextLine();
            System.out.println(line);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class hello  {
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        System.out.println(line);
+    }
+}
