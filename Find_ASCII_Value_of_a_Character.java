68357ce7728d172f99909fb3e926143bf1e52bff
syam suri <84723010+syamsuri09@users.noreply.github.com>
2023-06-06 10:38:42 +0530

    import java.util.*;
    public class arr {
        public static void main(String[] args){
            char c;
            Scanner sc = new Scanner(System.in);
            c = sc.next().charAt(0);
            int a = c;
    
            System.out.println(a);
        }
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class arr {
+    public static void main(String[] args){
+        char c;
+        Scanner sc = new Scanner(System.in);
+        c = sc.next().charAt(0);
+        int a = c;
+
+        System.out.println(a);
+    }
+}
