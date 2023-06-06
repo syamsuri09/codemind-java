76e94965e3f788a600f5bb2fd94caade7c826554
syam suri <84723010+syamsuri09@users.noreply.github.com>
2023-06-06 10:32:47 +0530

    import java.util.*;
    public class arr {
        public static void main(String[] args){
            float f,r;
    
            Scanner sc = new Scanner(System.in);
            f = sc.nextFloat();
            r = 32 + (f * 9/5);
            System.out.format("%.2f",r);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class arr {
+    public static void main(String[] args){
+        float f,r;
+
+        Scanner sc = new Scanner(System.in);
+        f = sc.nextFloat();
+        r = 32 + (f * 9/5);
+        System.out.format("%.2f",r);
+    }
+}
